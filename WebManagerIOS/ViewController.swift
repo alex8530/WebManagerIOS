d4f1447aee19dc9cba400780860433d217142618
Alex <Aliabuharb8530@gmail.com>
2020-12-04 11:59:58 -0800

    Initial Commit

---
+++ WebManagerIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebManagerIOS
+//
+//  Created by Alex on 12/4/20.
+//  Copyright © 2020 Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
